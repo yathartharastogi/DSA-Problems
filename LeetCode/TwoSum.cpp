69b2d77698e9fd4308c4600953afd74212dd510c
yras279 <yras279@2gmail.com>
2026-06-08 23:35:59 +0530

    Add LeetCode DSA problems

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem : Two Sum
+ * Platform : LeetCode #1
+ * Difficulty : Easy
+ * Topic : Arrays
+ *
+ * Time Complexity : O(n²)
+ * Space Complexity : O(1)
+ * Submitted by: Yathartha Rastogi
+ */
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j]==target){
+                    return{i,j};
+                }
+            }
+        }
+        return{};
+    }
+};
