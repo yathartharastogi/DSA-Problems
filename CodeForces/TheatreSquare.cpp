6ad5bb5400182ab7bc21bc6a79bdc9a9a1a778a4
yras279 <yras279@2gmail.com>
2026-06-11 00:55:49 +0530

    Added TheatreSqaure.cpp

---
+++ CodeForces/TheatreSquare.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem : Theatre Square
+ * Platform : Codeforces #1A
+ * Difficulty : Easy
+ * Topic : Math, Implementation
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * Submitted on : 11/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
+
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long m,n,a;
+    cin >> m >> n >> a;
+
+    long long r = (m + a - 1)/a;
+    long long c = (n + a - 1)/a;
+    cout<< r * c;
+    
+
+    return 0;
+}
+
