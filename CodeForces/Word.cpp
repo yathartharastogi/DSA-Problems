8c149a38e5c4a6d338c9c36364db6354772eb7eb
yathartharastogi <yras279@gmail.com>
2026-06-14 01:10:00 +0530

    Added Word.cpp

---
+++ CodeForces/Word.cpp
@@ -0,0 +1,52 @@
+/*
+ * Problem : A. Word
+ * Platform : Codeforces #59A
+ * Difficulty : Easy
+ * Topic : Strings, Implementation
+ *
+ * Time Complexity  : O(n)
+ * Space Complexity : O(1)
+ * Submitted on : 14/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
+
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string s;
+    int upper = 0;
+    int lower = 0;
+
+    cin >> s;
+
+    for (char c : s) {
+
+        if (isupper(c))
+            upper++;
+        
+        else
+            lower++;
+    }
+
+    if (upper > lower) {
+        
+        for (char &c : s) {
+            c = toupper(c);
+        }
+    }
+
+    else {
+    
+        for (char &c : s) {
+            c = tolower(c);
+        }
+    }
+
+    cout << s;
+
+    return 0;
+}
