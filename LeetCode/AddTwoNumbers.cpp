48c5776f4cd602196832514757602e076c0b7f89
yathartharastogi <yras279@gmail.com>
2026-06-14 22:52:34 +0530

    Added AddTwoNumbers.cpp

---
+++ LeetCode/AddTwoNumbers.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem : 2. Add Two Numbers
+ * Platform : LeetCode
+ * Difficulty : Medium
+ * Topic : Linked List, Math, Simulation
+ *
+ * Time Complexity  : O(max(n, m))
+ * Space Complexity : O(max(n, m))
+ * Submitted on : 14/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
+
+
+ class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        auto cur = &dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry +
+                     (l1 ? l1->val : 0) +
+                     (l2 ? l2->val : 0);
+
+            carry = sum / 10;
+            cur = cur->next = new ListNode(sum % 10);
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy.next;
+    }
+};
