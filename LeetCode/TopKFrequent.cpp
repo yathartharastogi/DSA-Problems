850dd99fa57fd96ef76c416010f6bf644d9d9baf
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:31 +0530

    docs(leetcode): add standardized comments and documentation to TopKFrequent.cpp

--- LeetCode/TopKFrequent.cpp
+++ LeetCode/TopKFrequent.cpp
@@ -10,31 +10,29 @@
  * Submitted by : Yathartha Rastogi
  */
 
-
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
+        // Map frequency of each unique number
         unordered_map<int, int> ump;
-
         for (int num : nums) {
             ump[num]++;
         }
 
+        // Bucket sort where index represents element frequency
         vector<vector<int>> bucket(nums.size() + 1);
-
-        for (auto& [num, count] : ump) {
+        for (auto& pair : ump) {
+            int num = pair.first;
+            int count = pair.second;
             bucket[count].push_back(num);
         }
 
         vector<int> ans;
 
+        // Traverse buckets from maximum frequency down to 0
         for (int i = nums.size(); i >= 0 && ans.size() < k; i--) {
-
             for (int num : bucket[i]) {
                 ans.push_back(num);
-
                 if (ans.size() == k) {
                     return ans;
                 }
