eeb9f7fa6a336943ab579a48cc16325c2908018f
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to Football.cpp

--- CodeForces/Football.cpp
+++ CodeForces/Football.cpp
@@ -6,39 +6,42 @@
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
- * Submitted on : 19/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     string s;
     cin >> s;
 
-    int n = s.length();
-    int count = 1;
+    int consecutive = 1;
+    bool dangerous = false;
 
-    for(int i = 1; i < n; i++){
-        if(s[i] == s[i-1]){
-            count++;
-
-            if(count >= 7){
-                cout << "YES";
-                return 0;
-            
+    // Check for 7 consecutive identical players ('0' or '1')
+    for (size_t i = 1; i < s.length(); i++) {
+        if (s[i] == s[i - 1]) {
+            consecutive++;
+            if (consecutive >= 7) {
+                dangerous = true;
+                break;
             }
-        }   else{
-            count = 1; 
+        } else {
+            consecutive = 1;
         }
-        
     }
 
-    cout << "NO";
-    return 0;
+    if (dangerous) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
 
+    return 0;
 }
