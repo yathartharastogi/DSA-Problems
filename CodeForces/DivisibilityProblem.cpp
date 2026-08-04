2a06a11b051a872caa7834f04b73b5667a9c1d7d
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to DivisibilityProblem.cpp

--- CodeForces/DivisibilityProblem.cpp
+++ CodeForces/DivisibilityProblem.cpp
@@ -1,21 +1,37 @@
+/*
+ * Problem : Divisibility Problem
+ * Platform : Codeforces #1328A
+ * Difficulty : Easy
+ * Topic : Math
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * Submitted on : 11/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     long long t;
     cin >> t;
-    long long count = 0;
 
-    while(t--){
-        long long a,b;
+    while (t--) {
+        long long a, b;
         cin >> a >> b;
 
-        if(a % b == 0){
-            cout << 0 << endl;
-        }
-        else{
-            cout << b - (a % b) << endl;
+        // If already divisible, 0 moves required
+        if (a % b == 0) {
+            cout << 0 << "\n";
+        } else {
+            // Distance to next multiple of b
+            cout << b - (a % b) << "\n";
         }
     }
+    return 0;
 }
