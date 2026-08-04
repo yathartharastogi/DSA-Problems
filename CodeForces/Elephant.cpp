f8b55a10094fa7778ba22ce35e4279c17e34cbaa
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to Elephant.cpp

--- CodeForces/Elephant.cpp
+++ CodeForces/Elephant.cpp
@@ -6,21 +6,24 @@
  *
  * Time Complexity  : O(1)
  * Space Complexity : O(1)
- * Submitted on : 12/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
-
- 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int n;
-    cin >> n;
+    int x;
+    cin >> x;
 
-    int a = (n+4)/5;
+    // Minimum 5-unit steps needed to reach position x
+    int steps = (x + 4) / 5;
 
-    cout << a;
+    cout << steps << "\n";
+    return 0;
 }
