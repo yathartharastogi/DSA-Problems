089abcf505cfd2499bb4be56c67b57f04bb42d9f
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:30 +0530

    docs(codeforces): add standardized header and inline comments to Word.cpp

--- CodeForces/Word.cpp
+++ CodeForces/Word.cpp
@@ -5,48 +5,45 @@
  * Topic : Strings, Implementation
  *
  * Time Complexity  : O(n)
- * Space Complexity : O(1)
- * Submitted on : 14/06/2026
+ * Space Complexity : O(n)
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
-
- 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     string s;
+    cin >> s;
+
     int upper = 0;
     int lower = 0;
 
-    cin >> s;
-
+    // Count uppercase vs lowercase characters
     for (char c : s) {
-
-        if (isupper(c))
+        if (isupper(c)) {
             upper++;
-        
-        else
+        } else {
             lower++;
-    }
-
-    if (upper > lower) {
-        
-        for (char &c : s) {
-            c = toupper(c);
         }
     }
 
-    else {
-    
+    // Convert to majority case (lowercase on tie)
+    if (upper > lower) {
+        for (char &c : s) {
+            c = toupper(c);
+        }
+    } else {
         for (char &c : s) {
             c = tolower(c);
         }
     }
 
-    cout << s;
-
+    cout << s << "\n";
     return 0;
 }
