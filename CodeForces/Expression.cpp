df6f1e51bef9c2458bb96a5b524b11cf52f06348
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to Expression.cpp

--- CodeForces/Expression.cpp
+++ CodeForces/Expression.cpp
@@ -1,11 +1,35 @@
+/*
+ * Problem : Expression
+ * Platform : Codeforces #479A
+ * Difficulty : Easy
+ * Topic : Math, Brute Force
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * Submitted on : 11/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int a,b,c;
+    int a, b, c;
     cin >> a >> b >> c;
+
+    // Evaluate all parenthesizations & operations to find maximum
     cout << max({
-        a*b*c, (a+b)*c, a*(b+c), a+b*c, a*b+c, a+b+c
-    }) <<endl;
+        a * b * c,
+        (a + b) * c,
+        a * (b + c),
+        a + b * c,
+        a * b + c,
+        a + b + c
+    }) << "\n";
+
+    return 0;
 }
