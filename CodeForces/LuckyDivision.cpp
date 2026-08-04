57cb1eee81d4b1482ebd638c1593f0091a89e318
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:28 +0530

    fix(codeforces): resolve code logic/syntax issue and add header comments to LuckyDivision.cpp

--- CodeForces/LuckyDivision.cpp
+++ CodeForces/LuckyDivision.cpp
@@ -1,38 +1,57 @@
+/*
+ * Problem : Lucky Division
+ * Platform : Codeforces #122A
+ * Difficulty : Easy
+ * Topic : Math, Number Theory, Brute Force
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * Submitted on : 11/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int main(){
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int n;
     cin >> n;
-    int orignal = n;
-    bool res = true;
-    while (n > 0) {
-        int digit = n % 10;
+
+    int temp = n;
+    bool isLuckyDigits = true;
+
+    // Check if n consists solely of lucky digits (4 and 7)
+    while (temp > 0) {
+        int digit = temp % 10;
         if (digit != 4 && digit != 7) {
-            res = false;
+            isLuckyDigits = false;
             break;
         }
-        n /= 10;
+        temp /= 10;
     }
 
-    vector<int> lucky  = {4,7,44,47,74,77,444,447,474,477,744,747,774,777};
+    // List of pre-defined lucky numbers up to 1000
+    vector<int> lucky = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
 
-    for(int x : lucky){
-        if(orignal % x == 0){
-            res = true;
+    bool isAlmostLucky = isLuckyDigits;
+
+    // Check if n is divisible by any lucky number
+    for (int x : lucky) {
+        if (n % x == 0) {
+            isAlmostLucky = true;
             break;
         }
-        else{
-            res = false;
-        }
     }
 
-    if(res){
-        cout << "YES"; 
+    if (isAlmostLucky) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
     }
-    else{ 
-        cout << "NO";
-    }
+
+    return 0;
 }
