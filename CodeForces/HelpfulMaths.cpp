297e3515ed462bd2943449e67422503f7bbe7337
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to HelpfulMaths.cpp

--- CodeForces/HelpfulMaths.cpp
+++ CodeForces/HelpfulMaths.cpp
@@ -6,38 +6,38 @@
  *
  * Time Complexity  : O(n log n)
  * Space Complexity : O(n)
- * Submitted on : 12/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
-
- 
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     string s;
     cin >> s;
 
-    vector<char> nums;
+    vector<int> nums;
 
-    for(char c : s){
-        if(c != '+'){
-            nums.push_back(c);
+    // Extract numbers between '+' signs
+    for (char c : s) {
+        if (c != '+') {
+            nums.push_back(c - '0');
         }
     }
 
-    sort(nums.begin(), nums.end()); 
+    // Sort numbers in non-decreasing order
+    sort(nums.begin(), nums.end());
 
-    cout << nums[0];
-
-    for(int i = 1; i < nums.size(); i++){
-        cout << '+' << nums[i];
+    // Print reordered expression formatted with '+'
+    for (size_t i = 0; i < nums.size(); i++) {
+        cout << nums[i] << (i == nums.size() - 1 ? "" : "+");
     }
+    cout << "\n";
 
     return 0;
-
 }
