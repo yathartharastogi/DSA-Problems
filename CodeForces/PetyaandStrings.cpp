76010f3f589492b5a3f20537e999ed9bfee6392e
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:28 +0530

    docs(codeforces): add standardized header and inline comments to PetyaandStrings.cpp

--- CodeForces/PetyaandStrings.cpp
+++ CodeForces/PetyaandStrings.cpp
@@ -6,28 +6,36 @@
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
- * Submitted on : 17/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
-
-
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     string s1, s2;
     cin >> s1 >> s2;
 
-    for (char &c : s1) c = tolower(c);
-    for (char &c : s2) c = tolower(c);
+    // Case-insensitive lexicographical comparison
+    for (size_t i = 0; i < s1.length(); i++) {
+        char c1 = tolower(s1[i]);
+        char c2 = tolower(s2[i]);
 
-    if (s1 < s2)
-        cout << -1;
-    else if (s1 > s2)
-        cout << 1;
-    else
-        cout << 0;
+        if (c1 < c2) {
+            cout << -1 << "\n";
+            return 0;
+        }
+        if (c1 > c2) {
+            cout << 1 << "\n";
+            return 0;
+        }
+    }
 
+    cout << 0 << "\n";
     return 0;
 }
