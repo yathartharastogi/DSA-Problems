e320a2780eba76eb42e41d4d643ca967abbda188
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:29 +0530

    docs(codeforces): add standardized header and inline comments to Translation.cpp

--- CodeForces/Translation.cpp
+++ CodeForces/Translation.cpp
@@ -1,19 +1,34 @@
+/*
+ * Problem : Translation
+ * Platform : Codeforces #41A
+ * Difficulty : Easy
+ * Topic : Strings, Implementation
+ *
+ * Time Complexity  : O(n)
+ * Space Complexity : O(n)
+ * Submitted on : 11/06/2026
+ * Submitted by : Yathartha Rastogi
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string t;
-    string s;
-    cin >> t;
-    cin >> s;
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    string s, t;
+    cin >> s >> t;
+
+    // Reverse second string to compare against first
     reverse(s.begin(), s.end());
 
-    if(s == t){
-        cout << "YES";
-    }
-    else{
-        cout << "NO";
+    if (s == t) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
     }
 
+    return 0;
 }
