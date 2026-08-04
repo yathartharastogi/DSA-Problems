8cffc4af67ac53b3adbcc393051827688dcdabd4
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to GeorgeandAccommodation.cpp

--- CodeForces/GeorgeandAccommodation.cpp
+++ CodeForces/GeorgeandAccommodation.cpp
@@ -6,30 +6,32 @@
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
- * Submitted on : 17/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
-
-
-
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
     cin >> n;
 
     int count = 0;
 
+    // Count rooms with at least 2 free spaces (capacity q - occupied p >= 2)
     while (n--) {
         int p, q;
         cin >> p >> q;
-
-        if (q - p >= 2)
+        if (q - p >= 2) {
             count++;
+        }
     }
 
-    cout << count;
+    cout << count << "\n";
     return 0;
 }
