9203ab90d745e9f41016543f2cee4731b151aa7f
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:29 +0530

    docs(codeforces): add standardized header and inline comments to StonesontheTable.cpp

--- CodeForces/StonesontheTable.cpp
+++ CodeForces/StonesontheTable.cpp
@@ -6,32 +6,33 @@
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
- * Submitted on : 19/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
+#include <bits/stdc++.h>
+using namespace std;
 
-
- #include<bits/stdc++.h>
- using namespace std;
-
- int main() {
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int n;
+    cin >> n;
+
     string s;
+    cin >> s;
 
-    cin >> n >> s;
+    int removals = 0;
 
-    int ans = 0;
-
-    for(int i = 0; i < n; i++){
-        if(s[i] == s[i-1]){
-
-            ans++;
+    // Count adjacent identical stones to calculate required removals
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) {
+            removals++;
         }
     }
 
-
-    cout << ans;
-
- }
+    cout << removals << "\n";
+    return 0;
+}
