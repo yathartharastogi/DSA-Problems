d275155c7bed36b771c8b322e040c6063e45be4b
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:27 +0530

    docs(codeforces): add standardized header and inline comments to Drinks.cpp

--- CodeForces/Drinks.cpp
+++ CodeForces/Drinks.cpp
@@ -6,32 +6,30 @@
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(1)
- * Submitted on : 18/06/2026
+ * Submitted on : 11/06/2026
  * Submitted by : Yathartha Rastogi
  */
 
- 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int n;
-    double sum = 0;
     cin >> n;
-    int total = n;
 
-    while(n--){
-        int p;
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        double p;
         cin >> p;
         sum += p;
-
     }
 
-    cout << sum/total;
+    // Calculate average volume fraction
+    cout << fixed << setprecision(12) << sum / n << "\n";
 
     return 0;
-
-
 }
