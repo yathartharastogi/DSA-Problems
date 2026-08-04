fcf9182eab0ed53b6453fe7b984fca64b68f9afb
yathartharastogi <yras279@gmail.com>
2026-08-05 00:04:31 +0530

    docs(leetcode): add standardized comments and documentation to MinStack.cpp

--- LeetCode/MinStack.cpp
+++ LeetCode/MinStack.cpp
@@ -10,34 +10,36 @@
  * Submitted by : Yathartha Rastogi
  */
 
-
- 
 class MinStack {
 public:
+    // Stack stores pairs: {element_value, current_minimum}
     stack<pair<int, int>> s;
 
     MinStack() {
         
     }
     
+    // Push element onto stack and record updated minimum
     void push(int value) {
-        if(s.empty()){
+        if (s.empty()) {
             s.push({value, value});
-        }
-        else{
+        } else {
             int minVal = min(value, s.top().second);
             s.push({value, minVal});
         }
     }
     
+    // Remove top element from stack
     void pop() {
-        s.pop()
+        s.pop();
     }
     
+    // Retrieve top element value
     int top() {
         return s.top().first;        
     }
     
+    // Retrieve minimum element in stack in O(1)
     int getMin() {
         return s.top().second;
     }
