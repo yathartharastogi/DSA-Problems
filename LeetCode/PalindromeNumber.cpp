8e9f04383592c3c640f6fef6060c4163df06efd2
yathartharastogi <yras279@gmail.com>
2026-08-06 11:12:57 +0530

    added leetcode solution

---
+++ LeetCode/PalindromeNumber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) return false;
+
+        int original = x;
+        int rev = 0;
+
+        while (x != 0) {
+            int last = x % 10;
+            rev = rev * 10 + last;
+            x /= 10;
+        }
+
+        return original == rev;
+    }
+}
